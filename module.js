52a1d9b2cf7cebc0d3b200c1f423095153f75f52
Nazia-Karim-Khan <nazia.karim@brainstation-23.com>
2024-07-01 11:24:54 +0600

    Alternative Syntax

--- module.js
+++ module.js
@@ -2,7 +2,9 @@ const names = require('./name');
 console.log(names);
 
 const sayHi = require('./utils');
+const data = require('./alternative-flavor');
 
+console.log(data);
 sayHi('susan');
 sayHi(names.john);
 sayHi(names.peter);
