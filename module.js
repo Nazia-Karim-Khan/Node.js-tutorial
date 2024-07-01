f6e3d2ec02e09af18b36d2afa12ca3fe5e272b75
Nazia-Karim-Khan <nazia.karim@brainstation-23.com>
2024-07-01 11:58:05 +0600

    Mind Grenade

--- module.js
+++ module.js
@@ -3,7 +3,7 @@ console.log(names);
 
 const sayHi = require('./utils');
 const data = require('./alternative-flavor');
-
+require('./mind-grenade');
 console.log(data);
 sayHi('susan');
 sayHi(names.john);
