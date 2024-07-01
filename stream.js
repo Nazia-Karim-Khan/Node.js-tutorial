f7336646681ce5b30bf747237cabdba0fdb979ac
Nazia-Karim-Khan <nazia.karim@brainstation-23.com>
2024-07-01 17:20:11 +0600

    stream

---
+++ stream.js
@@ -0,0 +1,14 @@
+const { createReadStream } = require('fs')
+
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
+const stream = createReadStream('./content/big.txt')
+
+stream.on('data', (result) => {
+//   console.log(result)
+  console.log(result.toString())
+})
+stream.on('error', (err) => console.log(err))
